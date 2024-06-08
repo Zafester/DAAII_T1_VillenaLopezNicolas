042f39bdd82c93a2a3c1ab085c053ded83d7b23a
Zafester <nickmanv2004@gmail.com>
2024-06-07 21:28:13 -0500

    commit configuration

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_VillenaLopezNicolas/configuration/AppConfig.java
@@ -0,0 +1,12 @@
+package pe.edu.cibertec.DAAII_T1_VillenaLopezNicolas.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+@Configuration
+public class AppConfig {
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
