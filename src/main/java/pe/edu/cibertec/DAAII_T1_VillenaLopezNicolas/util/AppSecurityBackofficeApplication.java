92dc3797feadcb8570ea6b0ce05d3fe879cb6b3c
Zafester <nickmanv2004@gmail.com>
2024-06-07 21:26:34 -0500

    commit util

---
+++ src/main/java/pe/edu/cibertec/DAAII_T1_VillenaLopezNicolas/util/AppSecurityBackofficeApplication.java
@@ -0,0 +1,11 @@
+package pe.edu.cibertec.DAAII_T1_VillenaLopezNicolas.util;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSecurityBackofficeApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AppSecurityBackofficeApplication.class, args);
+    }
+}
